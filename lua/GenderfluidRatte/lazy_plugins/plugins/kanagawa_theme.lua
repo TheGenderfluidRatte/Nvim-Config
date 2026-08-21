c085c477b8793c74c6aeba2e431afac87b1e7cd6
TheGenderfluidRatte <schwulratte@gmail.com>
2026-08-20 19:53:48 -0500

    mormatting

--- lua/GenderfluidRatte/lazy_plugins/plugins/kanagawa_theme.lua
+++ lua/GenderfluidRatte/lazy_plugins/plugins/kanagawa_theme.lua
@@ -1,3 +1,14 @@
 return {
-    "rebelot/kanagawa.nvim"
+    "rebelot/kanagawa.nvim",
+
+    lazy = false,
+    priority = 100,
+
+    config = function()
+        vim.api.nvim_create_autocmd("VimEnter", {
+            callback = function()
+                require("GenderfluidRatte.lazy_plugins.config.kanagawa_theme").setup()
+            end
+        })
+    end
 }
