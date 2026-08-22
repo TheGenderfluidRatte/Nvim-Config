0945337cd94484ee304495475b350b520308055e
TheGenderfluidRatte <schwulratte@gmail.com>
2026-08-21 20:17:22 -0500

    Implemented ruler, and icon to Statusline

--- lua/GenderfluidRatte/lazy_plugins/config/heirline/statusline.lua
+++ lua/GenderfluidRatte/lazy_plugins/config/heirline/statusline.lua
@@ -1,6 +1,9 @@
 local statusline = {
     require("GenderfluidRatte.lazy_plugins.config.heirline.statusline.mode_indicator").getStatusLineObj(),
     require("GenderfluidRatte.lazy_plugins.config.heirline.statusline.filename").getStatusLineObj(),
+    { provider = "%=" },
+    require("GenderfluidRatte.lazy_plugins.config.heirline.statusline.icon").getStatusLineObj(),
+    require("GenderfluidRatte.lazy_plugins.config.heirline.statusline.ruler").getStatusLineObj(),
 }
 
 return statusline
